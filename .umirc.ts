176a8428ef1a033c43a1920863749b7f67348c7e
18657134201 <1300950582@qq.com>
2021-12-15 15:41:37 +0800

    ddd

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  base: '/doc-test-demo/',
+  base: '/doc-test-demo',
   publicPath: '/doc-test-demo/',
   hash: true,  //加hash配置，清除缓存
   targets: {  // 浏览器兼容性，手机系统兼容性
